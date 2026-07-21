d07f2a391e7cdef4b0911e9b1e4c44131c7f11cf
Jackson <jacksonraj0711@gmail.com>
2026-07-21 19:18:08 +0530

    hi

--- backend/test_slice.cjs
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
-mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME }).then(async () => {
-  const Product = require('./products/models.js').default;
-  const docs = await Product.find({}).select('pid name reviews').slice('reviews', -2).limit(2).lean();
-  console.log(JSON.stringify(docs, null, 2));
-  mongoose.connection.close();
-});
